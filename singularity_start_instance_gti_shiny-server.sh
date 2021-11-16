08684b44329b4980da8f805d1d0456eb4fc5ace9
iferres <igferres@gmail.com>
2021-11-16 11:49:32 -0300

    run as instance

--- singularity_start_instance_gti_shiny-server.sh
+++ singularity_start_instance_gti_shiny-server.sh
@@ -2,10 +2,10 @@
 
 mkdir -p /tmp/shinylog 
 mkdir -p /tmp/shinybookmark
-singularity run \
+singularity instance start \
 --bind shiny:/srv/shiny-server \
 --bind /tmp/shinylog:/var/log/shiny-server/ \
 --bind /tmp/shinybookmark:/var/lib/shiny-server/ \
 --bind /export/home/$USER/Downloads \
 --bind shiny-server.conf:/etc/shiny-server/shiny-server.conf \
-/export/home/$USER/gti.sif
+/export/home/$USER/gti.sif gti
