62dce44e6595e9ff9e425bfce82ecefe9f97b3df
iferres <igferres@gmail.com>
2021-11-16 10:49:44 -0300

    bugfix

--- singularity_run_shiny-server.sh
+++ singularity_run_shiny-server.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-mkdir -p /export/home/$USER/shinylog 
-mkdir -p /export/home/$USER/shinybookmark
-singularity run \ 
---bind GTi_UY_shiny:/srv/shiny-server \ 
---bind /export/home/$USER/shinylog:/var/log/shiny-server/ \
---bind /export/home/$USER/shinybookmark:/var/lib/shiny-server/ \
+mkdir -p /tmp/shinylog 
+mkdir -p /tmp/shinybookmark
+singularity run \
+--bind shiny:/srv/shiny-server \
+--bind /tmp/shinylog:/var/log/shiny-server/ \
+--bind /tmp/shinybookmark:/var/lib/shiny-server/ \
 --bind /export/home/$USER/Downloads \
 --bind shiny-server.conf:/etc/shiny-server/shiny-server.conf \
 /export/home/$USER/gti.sif
