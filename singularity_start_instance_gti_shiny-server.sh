ce8ae0b565876ffe9ab740addef9b3e54fa67893
iferres <igferres@gmail.com>
2022-03-17 17:37:47 -0300

    fixed paths /export/... to /home/...

--- singularity_start_instance_gti_shiny-server.sh
+++ singularity_start_instance_gti_shiny-server.sh
@@ -7,6 +7,6 @@ singularity instance start \
 --bind shiny:/srv/shiny-server \
 --bind /tmp/shinylog:/var/log/shiny-server/ \
 --bind /tmp/shinybookmark:/var/lib/shiny-server/ \
---bind /export/home/$USER/Downloads \
+--bind /home/$USER/Downloads \
 --bind shiny-server.conf:/etc/shiny-server/shiny-server.conf \
-/export/home/$USER/gti.sif gti
+/home/$USER/gti.sif gti
